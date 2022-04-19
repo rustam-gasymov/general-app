0b754e4f41d95c7b85b23f9c517ca8b827f65d33
rustam-cloud <gasymovrustam86@Yandex.com>
2022-04-19 07:40:14 +0300

    feat: s

--- src/components/Decrement/Decrement.tsx
+++ src/components/Decrement/Decrement.tsx
@@ -1,16 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 
 export const Decrement = () => {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
+  // const state = useSelector((state) => state);
+  // const dispatch = useDispatch();
 
-  console.log(state);
-  console.log("dispatch");
+  // console.log(state);
+  // console.log("dispatch");
 
   const handleClick = () => {
     console.log("hello world");
     //@ts-ignore
-    dispatch(setDecrementValue());
+    // dispatch(setDecrementValue());
   };
 
   return <button onClick={handleClick}>Decrement</button>;
